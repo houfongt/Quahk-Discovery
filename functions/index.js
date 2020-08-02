161d5bac24788cda6b07395d84d9932215b18c33
Edward Swalker <edwardtang0611@gmail.com>
2020-08-02 21:16:24 +0800

    Updates

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,19 @@
 const functions = require('firebase-functions');
+const nodemailer = require('nodemailer');
+const cors = require('cors')({ origin: true });
+
+//gmail account
+const gmailEmail = functions.config().gmail.email;
+const gmailPassword = functions.config().gmail.password;
+
+// the mail
+const mailTransport = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: gmailEmail,
+        pass: gmailPassword
+    }
+});
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -7,3 +22,23 @@ const functions = require('firebase-functions');
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+exports.formSubmit = functions.https.onRequest((req, res) => {
+    cors(req, res, () => {
+        if (req.method !== 'POST') {
+            return;
+        }
+
+        const mailOptions = {
+            from: gmailEmail,
+            to: req.body.email,
+            subject: `from my website ${req.body.email}`,
+            text: req.body.message,
+            html: `<p>${req.body.message}`
+        };
+
+        mailTransport.sendMail(mailOptions);
+
+        res.status(200).send({ isEmailSend: true });
+    });
+}); 
