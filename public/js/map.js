f16ac241023ce6ec14f05b02ca72abc0dc7575e9
Edward <edwardtang0611@gmail.com>
2020-08-19 18:30:58 +0800

    Code formatting

--- public/js/map.js
+++ public/js/map.js
@@ -1,155 +1,226 @@
-var LocationBtn = document.getElementById('autoLocationBtn');
-
-  
-  
 function startMap() {
-    $('#mapModel').modal('open');
-    initMap();
-  }
+  $("#mapModel").modal("open");
+  initMap();
+}
 
-  // Note: This example requires that you consent to location sharing when
-      // prompted by your browser. If you see the error "The Geolocation service
-      // failed.", it means you probably did not give permission for the browser to
-      // locate you.
 var map, infoWindow;
-      //var marker = false;
-    
-function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 22.210928, lng: 113.552971},
-        zoom: 18
-    });
-        //infoWindow = new google.maps.Marker;
 
-        // Try HTML5 geolocation.
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 22.210928, lng: 113.552971 },
+    zoom: 18,
+  });
+
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      function (position) {
         var pos = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
         };
 
         var marker = new google.maps.Marker({
-            position: pos,
-            map: map,
-            draggable: true //make it draggable
+          position: pos,
+          map: map,
+          draggable: true, //make it draggable
         });
-               // console.log(marker);
-            //infoWindow.setContent('Location found.');
-            //infoWindow.open(map);
+        // console.log(marker);
+        //infoWindow.setContent('Location found.');
+        //infoWindow.open(map);
         map.setCenter(pos);
 
         markerLocation(marker);
 
-        google.maps.event.addListener(map, 'click', function(event) {
-        
-            var clickedLocation = event.latLng;
-            marker.setPosition(clickedLocation);
-            markerLocation(marker);
+        google.maps.event.addListener(map, "click", function (event) {
+          var clickedLocation = event.latLng;
+          marker.setPosition(clickedLocation);
+          markerLocation(marker);
         });
 
-        google.maps.event.addListener(marker, 'dragend', function(event){
-            markerLocation(marker);
+        google.maps.event.addListener(marker, "dragend", function (event) {
+          markerLocation(marker);
         });
-
-        }, function(error) {
-            if (error.code == error.PERMISSION_DENIED) {
-                $('#mapError').modal('open');
-                var marker = false;
-                google.maps.event.addListener(map, 'click', function(event) {                
-                    //Get the location that the user clicked.
-                    var clickedLocation = event.latLng;
-                    //If the marker hasn't been added.
-                    if(marker == false){
-                        //Create the marker.
-                        marker = new google.maps.Marker({
-                            position: clickedLocation,
-                            map: map,
-                            draggable: true //make it draggable
-                        });
-                        //Listen for drag events!
-                        google.maps.event.addListener(marker, 'dragend', function(event){
-                            markerLocation(marker);
-                        });
-                    } else{
-                        //Marker has already been added, so just change its location.
-                        marker.setPosition(clickedLocation);
-                    }
-                    //Get the marker's location.
-                    markerLocation(marker);
-                }); 
-            }
-          });
-        } else {
-          // Browser doesn't support Geolocation
-          //$('#mapError').modal('open');
-            
-          //handleLocationError(false, infoWindow, map.getCenter());
-        }
-        /*
-        google.maps.event.addListener(map, 'click', function(event) {                
+      },
+      function (error) {
+        if (error.code == error.PERMISSION_DENIED) {
+          document.getElementById("mapErrorReason").innerText =
+            "錯誤原因：用戶拒絕";
+          $("#mapError").modal("open");
+          var marker = false;
+          google.maps.event.addListener(map, "click", function (event) {
             //Get the location that the user clicked.
             var clickedLocation = event.latLng;
             //If the marker hasn't been added.
-            if(marker == false){
-                //Create the marker.
-                marker = new google.maps.Marker({
-                    position: clickedLocation,
-                    map: map,
-                    draggable: true //make it draggable
-                });
-                //Listen for drag events!
-                google.maps.event.addListener(marker, 'dragend', function(event){
-                    markerLocation();
-                });
-            } else{
-                //Marker has already been added, so just change its location.
-                marker.setPosition(clickedLocation);
+            if (marker == false) {
+              //Create the marker.
+              marker = new google.maps.Marker({
+                position: clickedLocation,
+                map: map,
+                draggable: true, //make it draggable
+              });
+              //Listen for drag events!
+              google.maps.event.addListener(marker, "dragend", function (
+                event
+              ) {
+                markerLocation(marker);
+              });
+            } else {
+              //Marker has already been added, so just change its location.
+              marker.setPosition(clickedLocation);
             }
             //Get the marker's location.
-            markerLocation();
-        }); */
+            markerLocation(marker);
+          });
+        } else if (error.code == error.POSITION_UNAVAILABLE) {
+          document.getElementById("mapErrorReason").innerText =
+            "錯誤原因：用戶已批准，但裝置內部零件發生問題。";
+          $("#mapError").modal("open");
+          var marker = false;
+          google.maps.event.addListener(map, "click", function (event) {
+            //Get the location that the user clicked.
+            var clickedLocation = event.latLng;
+            //If the marker hasn't been added.
+            if (marker == false) {
+              //Create the marker.
+              marker = new google.maps.Marker({
+                position: clickedLocation,
+                map: map,
+                draggable: true, //make it draggable
+              });
+              //Listen for drag events!
+              google.maps.event.addListener(marker, "dragend", function (
+                event
+              ) {
+                markerLocation(marker);
+              });
+            } else {
+              //Marker has already been added, so just change its location.
+              marker.setPosition(clickedLocation);
+            }
+            //Get the marker's location.
+            markerLocation(marker);
+          });
+        } else if (error.code == error.TIMEOUT) {
+          document.getElementById("mapErrorReason").innerText =
+            "錯誤原因：用戶已批准，獲取逾時";
+          $("#mapError").modal("open");
+          var marker = false;
+          google.maps.event.addListener(map, "click", function (event) {
+            //Get the location that the user clicked.
+            var clickedLocation = event.latLng;
+            //If the marker hasn't been added.
+            if (marker == false) {
+              //Create the marker.
+              marker = new google.maps.Marker({
+                position: clickedLocation,
+                map: map,
+                draggable: true, //make it draggable
+              });
+              //Listen for drag events!
+              google.maps.event.addListener(marker, "dragend", function (
+                event
+              ) {
+                markerLocation(marker);
+              });
+            } else {
+              //Marker has already been added, so just change its location.
+              marker.setPosition(clickedLocation);
+            }
+            //Get the marker's location.
+            markerLocation(marker);
+          });
+        }
       }
-
-    
-      /*
-      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(browserHasGeolocation ?
-                              'Error: The Geolocation service failed.' :
-                              'Error: Your browser doesn\'t support geolocation.');
-        infoWindow.open(map);
+    );
+  } else {
+    // Browser doesn't support Geolocation
+    $("#mapError").modal("open");
+    var marker = false;
+    google.maps.event.addListener(map, "click", function (event) {
+      //Get the location that the user clicked.
+      var clickedLocation = event.latLng;
+      //If the marker hasn't been added.
+      if (marker == false) {
+        //Create the marker.
+        marker = new google.maps.Marker({
+          position: clickedLocation,
+          map: map,
+          draggable: true, //make it draggable
+        });
+        //Listen for drag events!
+        google.maps.event.addListener(marker, "dragend", function (event) {
+          markerLocation(marker);
+        });
+      } else {
+        //Marker has already been added, so just change its location.
+        marker.setPosition(clickedLocation);
       }
-      */
-    function markerLocation(marker){
-        //Get location.
-        var currentLocation = marker.getPosition();
-        //Add lat and lng values to a field that we can save.
-        let lat = currentLocation.lat();
-        let lng = currentLocation.lng();
-        let finalPosition = lat + "," + lng;
-        console.log(finalPosition);
-        document.getElementById('input_location').value = finalPosition;
-        
-        var geocoder = new google.maps.Geocoder;
-        var decodedName = document.getElementById('decodedName');
+      //Get the marker's location.
+      markerLocation(marker);
+    });
+  }
+  /*
+    google.maps.event.addListener(map, 'click', function(event) {                
+        //Get the location that the user clicked.
+        var clickedLocation = event.latLng;
+        //If the marker hasn't been added.
+        if(marker == false){
+            //Create the marker.
+            marker = new google.maps.Marker({
+                position: clickedLocation,
+                map: map,
+                draggable: true //make it draggable
+            });
+            //Listen for drag events!
+            google.maps.event.addListener(marker, 'dragend', function(event){
+                markerLocation();
+            });
+        } else{
+            //Marker has already been added, so just change its location.
+            marker.setPosition(clickedLocation);
+        }
+        //Get the marker's location.
+        markerLocation();
+    }); */
+}
+/*
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+    'Error: The Geolocation service failed.' :
+    'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(map);
+}
+*/
+function markerLocation(marker) {
+  //Get location.
+  var currentLocation = marker.getPosition();
+  //Add lat and lng values to a field that we can save.
+  let lat = currentLocation.lat();
+  let lng = currentLocation.lng();
+  let finalPosition = lat + "," + lng;
+  //console.log(finalPosition);
+  document.getElementById("input_location").value = finalPosition;
 
-        geocodeLatLng(geocoder, finalPosition);
-    }
+  var geocoder = new google.maps.Geocoder();
+
+  geocodeLatLng(geocoder, finalPosition);
+}
 
 function geocodeLatLng(geocoder, finalPosition) {
-    
-    var latlngStr = finalPosition.split(',', 2);
-    var latlng = {lat: parseFloat(latlngStr[0]), lng: parseFloat(latlngStr[1])};
-    geocoder.geocode({'location': latlng}, function(results, status) {
-        if (status === 'OK') {
-            if (results[0]) {
-              document.getElementById('decoded_location').value = results[0].formatted_address;
-            } else {
-              window.alert('No results found');
-            }
-          } else {
-            window.alert('Geocoder failed due to: ' + status);
-          }
-        });
+  var latlngStr = finalPosition.split(",", 2);
+  var latlng = { lat: parseFloat(latlngStr[0]), lng: parseFloat(latlngStr[1]) };
+  geocoder.geocode({ location: latlng }, function (results, status) {
+    if (status === "OK") {
+      if (results[0]) {
+        document.getElementById("decoded_location").value =
+          results[0].formatted_address;
+      } else {
+        window.alert("No results found");
       }
+    } else {
+      window.alert("Geocoder failed due to: " + status);
+    }
+  });
+}
--- public/js/switchPage.js
+++ public/js/switchPage.js
@@ -1,38 +1,40 @@
 // define all the elements
-let backForm1Btn = $('#backForm1Btn'),
-    loaderWrapper = $('.loader-wrapper'),
-    form2Options = document.getElementById('form2OptionsId'),
-    toform3Btn = document.getElementById('form2Next'),
-    toForm2Btn = document.getElementById('form1Next'),
-    form2 = document.getElementsByClassName('form2')[0],
-    form1 = document.getElementsByClassName('form1')[0],
-    formCancelBtn = document.getElementById('formCancelBtn');
+let backForm1Btn = $("#backForm1Btn"),
+  loaderWrapper = $(".loader-wrapper"),
+  form2Options = document.getElementById("form2OptionsId"),
+  toform3Btn = document.getElementById("form2Next"),
+  toForm2Btn = document.getElementById("form1Next"),
+  form2 = document.getElementsByClassName("form2")[0],
+  form1 = document.getElementsByClassName("form1")[0],
+  formCancelBtn = document.getElementById("formCancelBtn");
 
 // back to form 1
-backForm1Btn.on('click', function(e) {
-    form2Options.style.display = 'none';
-    toform3Btn.style.display = 'none';
-    loaderWrapper.fadeIn('fast');
-    form2.style.display = 'none';
-    form1.style.display = 'inline-block';
-    formCancelBtn.style.display = 'inline';
-    toForm2Btn.style.display = 'inline';
-    loaderWrapper.fadeOut('slow');
+backForm1Btn.on("click", function (e) {
+  form2Options.style.display = "none";
+  toform3Btn.style.display = "none";
+  loaderWrapper.fadeIn("fast");
+  form2.style.display = "none";
+  form1.style.display = "inline-block";
+  formCancelBtn.style.display = "inline";
+  toForm2Btn.style.display = "inline";
+  loaderWrapper.fadeOut("slow");
 });
 
 // go to form 2
-$('#form1Next').on('click', function(e) {
-
-    if (document.getElementById('store_name').value == "" || document.getElementById('input_location').value == "") {
-        $('#missingInfoModel').modal('open');
-    } else {
-        formCancelBtn.style.display = 'none';
-        toForm2Btn.style.display = 'none';
-        loaderWrapper.fadeIn('fast');
-        form1.style.display = 'none';
-        form2.style.display = 'inline-block';
-        form2Options.style.display = 'inline';
-        toform3Btn.style.display = 'inline';
-        loaderWrapper.fadeOut('slow');
-    }
+$("#form1Next").on("click", function (e) {
+  if (
+    document.getElementById("store_name").value == "" ||
+    document.getElementById("input_location").value == ""
+  ) {
+    $("#missingInfoModel").modal("open");
+  } else {
+    formCancelBtn.style.display = "none";
+    toForm2Btn.style.display = "none";
+    loaderWrapper.fadeIn("fast");
+    form1.style.display = "none";
+    form2.style.display = "inline-block";
+    form2Options.style.display = "inline";
+    toform3Btn.style.display = "inline";
+    loaderWrapper.fadeOut("slow");
+  }
 });
