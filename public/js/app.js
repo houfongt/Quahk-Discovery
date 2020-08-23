6bb1e8ea1e722887025b80ef7303237b3572c5e9
Edward <edwardtang0611@gmail.com>
2020-08-23 18:27:20 +0800

    Add some vuejs for displaying posts

---
+++ public/js/app.js
@@ -0,0 +1,30 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    merchants: [],
+    merchantsImage: []
+  },
+  mounted() {
+    const merchantRef = firebase.firestore().collection('merchants');
+    merchantRef.onSnapshot((snapshot) => {
+      let merchants = [];
+      snapshot.forEach((doc) => {
+        merchants.push({ ...doc.data(), id: doc.id });
+
+        const merchantImgRef = firebase.firestore().collection('merchants').doc(doc.id).collection('images');
+        merchantImgRef.onSnapshot((snapshot) => {
+          let merchantsImage = [];
+          snapshot.forEach((doc) => {
+              merchantsImage.push({...doc.data(), id:doc.id});
+
+              
+          });
+
+          this.merchantsImage = merchantsImage;
+        });
+      });
+
+      this.merchants = merchants;
+    });
+  },
+});
