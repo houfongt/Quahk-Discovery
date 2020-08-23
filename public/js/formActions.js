ff928c2de0e4ac1200f6d33a4b8d4ee8557950b6
Edward <edwardtang0611@gmail.com>
2020-08-23 15:38:02 +0800

    Update the UI after submitting the form

--- public/js/formActions.js
+++ public/js/formActions.js
@@ -83,30 +83,41 @@ finalSubmit.on('click', function (e) {
       console.log('Document written with ID: ', docRef.id);
       let docId = docRef.id;
       console.log(document.getElementById('photosUpload').files.length);
-      for (let i = 0; i < document.getElementById('photosUpload').files.length; i++) {
-        let imageName =  Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-        let imageFile = document.getElementById('photosUpload').files[i];
-        var storage = firebase.storage();
-        var imagesRef = storage.ref('images/' + imageName);
+      if (document.getElementById('photosUpload').files.length > 0) {
+        for (let i = 0; i < document.getElementById('photosUpload').files.length; i++) {
+          let imageName = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+          let imageFile = document.getElementById('photosUpload').files[i];
+          var storage = firebase.storage();
+          var imagesRef = storage.ref('images/' + imageName);
 
-        var file = imagesRef.put(imageFile);
+          var file = imagesRef.put(imageFile);
 
-        console.log(docId);
+          console.log(docId);
 
-        file.on('state_changed', function complete() {
-          file.snapshot.ref.getDownloadURL().then(function (url) {
-            //console.log(downloadURL);
-            firestore.collection('merchants').doc(docId).collection('images').add({
-                imageSrc: url,
-              })
-              .then(function (docRef) {
-                console.log('Document written with ID: ', docRef.id);
-              })
-              .catch(function (error) {
-                console.error('Error adding document: ', error);
-              });
+          file.on('state_changed', function complete() {
+            file.snapshot.ref.getDownloadURL().then(function (url) {
+              //console.log(downloadURL);
+              firestore
+                .collection('merchants')
+                .doc(docId)
+                .collection('images')
+                .add({
+                  imageSrc: url,
+                })
+                .then(function (docRef) {
+                  console.log('Document written with ID: ', docRef.id);
+                  $('.closeBtn_Actions').trigger('click');
+                  M.toast({ html: '成功加入記錄' });
+                })
+                .catch(function (error) {
+                  console.error('Error adding document: ', error);
+                });
+            });
           });
-        });
+        }
+      } else {
+        $('.closeBtn_Actions').trigger('click');
+        M.toast({ html: '成功加入記錄' });
       }
     })
     .catch(function (error) {
