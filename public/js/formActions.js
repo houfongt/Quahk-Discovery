aa10b3b311d54bca35b0ad7f14f00dd3bee31c0b
Edward Swalker <edwardtang0611@gmail.com>
2020-08-25 17:27:56 +0800

    Image upload bug fix

--- public/js/formActions.js
+++ public/js/formActions.js
@@ -94,14 +94,42 @@ finalSubmit.on('click', function (e) {
 
           console.log(docId);
 
-          file.on('state_changed', function complete() {
+          file.on(firebase.storage.TaskEvent.STATE_CHANGED,function (snapshot) {
+              // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+              var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+              console.log('Upload is ' + progress + '% done');
+              switch (snapshot.state) {
+                case firebase.storage.TaskState.PAUSED: // or 'paused'
+                  console.log('Upload is paused');
+                  break;
+                case firebase.storage.TaskState.RUNNING: // or 'running'
+                  console.log('Upload is running');
+                  break;
+              }
+            }, function (error) {
+
+              // A full list of error codes is available at
+              // https://firebase.google.com/docs/storage/web/handle-errors
+              switch (error.code) {
+                case 'storage/unauthorized':
+                  // User doesn't have permission to access the object
+                  break;
+
+                case 'storage/canceled':
+                  // User canceled the upload
+                  break;
+
+    
+
+    case 'storage/unknown':
+      // Unknown error occurred, inspect error.serverResponse
+      break;
+        }
+      }
+            , function() {
             file.snapshot.ref.getDownloadURL().then(function (url) {
               //console.log(downloadURL);
-              firestore
-                .collection('merchants')
-                .doc(docId)
-                .collection('images')
-                .add({
+              firestore.collection('merchants').doc(docId).collection('images').add({
                   imageSrc: url,
                 })
                 .then(function (docRef) {
