c8fa73851cb34b72847e3457b0f9d194ce3ee9f5
Edward <edwardtang0611@gmail.com>
2020-08-28 02:02:31 +0800

    Post page init release

--- public/js/app.js
+++ public/js/app.js
@@ -1,22 +1,21 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   var elems = document.getElementById('welcomeModal');
-  var instances = M.Modal.init(elems, {dismissible: false});
+  var instances = M.Modal.init(elems, { dismissible: false });
 });
 
-Vue.directive("trim", {
-  inserted: function(el) {
+Vue.directive('trim', {
+  inserted: function (el) {
     var str = el.innerHTML;
-    var resultString = str.split('').slice(0, 15).join("") + "...";
-    el.innerHTML = resultString
-  }
+    var resultString = str.split('').slice(0, 15).join('') + '...';
+    el.innerHTML = resultString;
+  },
 });
-
 
 var indexApp = new Vue({
   el: '#app',
   data: {
     merchants: [],
-    merchantsImage: []
+    merchantsImage: [],
   },
   mounted() {
     const merchantRef = firebase.firestore().collection('merchants');
@@ -29,9 +28,7 @@ var indexApp = new Vue({
         merchantImgRef.onSnapshot((snapshot) => {
           let merchantsImage = [];
           snapshot.forEach((doc) => {
-              merchantsImage.push({...doc.data(), id:doc.id});
-
-              
+            merchantsImage.push({ ...doc.data(), id: doc.id });
           });
 
           this.merchantsImage = merchantsImage;
@@ -43,10 +40,8 @@ var indexApp = new Vue({
   },
 });
 
-
 function firstLaunch() {
   if (localStorage.getItem('firstLaunch') == 'true') {
-    
   } else {
     $('#welcomeModal').modal('open');
   }
--- public/js/postsApp.js
+++ public/js/postsApp.js
@@ -1,19 +1,28 @@
-console.log(location.search)
+console.log(location.search);
 let query = location.search;
 let queryfinal = query.split('?');
-console.log(queryfinal[1])
+console.log(queryfinal[1]);
 
-document.getElementById('title').innerHTML = (window.location.search).split('?')[1];
+document.getElementById('title').innerHTML = window.location.search.split('?')[1];
 
 var postsApp = new Vue({
-    el: '#postHtml',
-    data: {
-      merchantId: (window.location.search).split('?')[1],
-      merchants: []
-    },
-    mounted() {
-        
-        const merchantRef = firebase.firestore().collection('merchants').doc(merchantId);
-        console.log(merchantRef)
-    }
-  });
+  el: '#postHtml',
+  data: {
+    merchantId: window.location.search.split('?')[1],
+    merchantData: [],
+  },
+  mounted() {
+    let docId = window.location.search.split('?')[1];
+    const merchantRef = firebase.firestore().collection('merchants').doc(docId);
+    console.log(merchantRef);
+    merchantRef.onSnapshot((doc) => {
+      let merchantData = [];
+
+      merchantData.push({ ...doc.data(), id: doc.id });
+
+      this.merchantData = merchantData;
+    });
+
+    
+  },
+});
