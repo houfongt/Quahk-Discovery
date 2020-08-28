fe02c85785a51dd873ce0b2b8f73d75dad49fe6b
Edward <edwardtang0611@gmail.com>
2020-08-29 01:01:37 +0800

    Restructure

--- public/js/formActions.js
+++ public/js/formActions.js
@@ -8,7 +8,8 @@ let star1 = $('#star1'),
   sendCopyBox = $('#sendCopyBox'),
   finalSubmit = $('#finalSubmit'),
   emailRating = $('#emailRating'),
-  rating = "該用戶沒有評";
+  uploadStatus = false,
+  rating = '該用戶沒有評';
 
 star1.on('click', (e) => {
   e.preventDefault();
@@ -90,6 +91,7 @@ finalSubmit.on('click', function (e) {
   let geopoint = document.getElementById('input_location').value.split(',', 2);
   let lat = parseFloat(geopoint[0]);
   let lng = parseFloat(geopoint[1]);
+  let imagesURLPath = [];
   let decodedLocation = document.getElementById('decoded_location').value;
   if (document.getElementById('textarea1').value !== '') {
     window.merchantComments = document.getElementById('textarea1').value;
@@ -97,6 +99,61 @@ finalSubmit.on('click', function (e) {
     window.merchantComments = '該用戶沒有評論';
   }
   let firestore = firebase.firestore();
+
+  if (document.getElementById('photosUpload').files.length > 0) {
+    for (let i = 0; i < document.getElementById('photosUpload').files.length; i++) {
+     
+      let imageRefName = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+      let imageFile = document.getElementById('photosUpload').files[i];
+      //let imageName = 'imageSrc' + i;
+      var storage = firebase.storage();
+      var imagesRef = storage.ref('images/' + imageRefName);
+
+      var file = imagesRef.put(imageFile);
+
+      imagesURLPath[i] = 'images/' + imageRefName;
+
+      file.on(
+        firebase.storage.TaskEvent.STATE_CHANGED,
+        function (snapshot) {
+          // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+          var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log('Upload is ' + progress + '% done');
+          switch (snapshot.state) {
+            case firebase.storage.TaskState.PAUSED: // or 'paused'
+              console.log('Upload is paused');
+              break;
+            case firebase.storage.TaskState.RUNNING: // or 'running'
+              console.log('Upload is running');
+              break;
+          }
+        },
+        function (error) {
+          // A full list of error codes is available at
+          // https://firebase.google.com/docs/storage/web/handle-errors
+          switch (error.code) {
+            case 'storage/unauthorized':
+              // User doesn't have permission to access the object
+              break;
+
+            case 'storage/canceled':
+              // User canceled the upload
+              break;
+
+            case 'storage/unknown':
+              // Unknown error occurred, inspect error.serverResponse
+              break;
+          }
+        },
+        function () {
+          console.log(imagesURLPath);
+        }
+      );
+    }
+  } else {
+    $('.closeBtn_Actions').trigger('click');
+    M.toast({ html: '成功加入記錄' });
+  }
 
   firestore
     .collection('merchants')
@@ -111,93 +168,37 @@ finalSubmit.on('click', function (e) {
     .then(function (docRef) {
       console.log('Document written with ID: ', docRef.id);
       let docId = docRef.id;
-      console.log(document.getElementById('photosUpload').files.length);
-      if (document.getElementById('photosUpload').files.length > 0) {
-        for (let i = 0; i < document.getElementById('photosUpload').files.length; i++) {
-          let imageRefName = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-          let imageFile = document.getElementById('photosUpload').files[i];
+      if (imagesURLPath[0] != undefined) {
+        let storageRef = firebase.storage().ref();
+        for (let i = 0; i < imagesURLPath.length; i++) {
+          let firestore = firebase.firestore();
           let imageName = 'imageSrc' + i;
-          var storage = firebase.storage();
-          var imagesRef = storage.ref('images/' + imageRefName);
-
-          var file = imagesRef.put(imageFile);
-
-          console.log(docId);
-
-          file.on(
-            firebase.storage.TaskEvent.STATE_CHANGED,
-            function (snapshot) {
-              // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-              var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-              console.log('Upload is ' + progress + '% done');
-              switch (snapshot.state) {
-                case firebase.storage.TaskState.PAUSED: // or 'paused'
-                  console.log('Upload is paused');
-                  break;
-                case firebase.storage.TaskState.RUNNING: // or 'running'
-                  console.log('Upload is running');
-                  break;
-              }
-            },
-            function (error) {
-              // A full list of error codes is available at
-              // https://firebase.google.com/docs/storage/web/handle-errors
-              switch (error.code) {
-                case 'storage/unauthorized':
-                  // User doesn't have permission to access the object
-                  break;
-
-                case 'storage/canceled':
-                  // User canceled the upload
-                  break;
-
-                case 'storage/unknown':
-                  // Unknown error occurred, inspect error.serverResponse
-                  break;
-              }
-            },
-            function () {
-              file.snapshot.ref.getDownloadURL().then(function (url) {
-                //console.log(downloadURL);
-                firestore
-                  .collection('merchants')
-                  .doc(docId)
-                  .update({
-                    [imageName]: url,
-                  })
-                  .then(function () {
-                    console.log('Document written with ID: ');
-                    
-                  })
-                  .catch(function (error) {
-                    console.error('Error adding document: ', error);
-                  });
-              });
-            }
-          );
-
-          if ((i + 1) == document.getElementById('photosUpload').files.length) {
-            /*
-            if (document.getElementById('sendCopyBox').checked) {
-              document.getElementsByClassName('form-page')[0].submit();
-            } else {
-              $('.closeBtn_Actions').trigger('click');
-              M.toast({ html: '成功加入記錄' });
-            }
-            */
-           console.log('true');
-          } else {
-            console.log('false');
-          }
+          storageRef.child(imagesURLPath[i]).getDownloadURL().then(function(url) {
+            firestore
+            .collection('merchants')
+            .doc(docId)
+            .update({
+              [imageName]: url
+            })
+            .then(function () {
+              console.log('Document successfully updated!');
+            })
+            .catch(function (error) {
+              // The document probably doesn't exist.
+              console.error('Error updating document: ', error);
+            });
+          })
+          
+          
         }
       } else {
-        $('.closeBtn_Actions').trigger('click');
-        M.toast({ html: '成功加入記錄' });
       }
     })
     .catch(function (error) {
       console.error('Error adding document: ', error);
     });
+
+  console.log('hello world');
 });
 
 function emailIsValid(email) {
